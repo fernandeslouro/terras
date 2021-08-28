21097aae433dec1496ea1dbabb510b85f4e831cc
fernandeslouro <jdlouro@gmv.com>
2021-08-28 22:22:25 +0100

    change markdown refresh to own function

--- create_markdown.py
+++ create_markdown.py
@@ -1,10 +1,8 @@
 # %% 
 from types import MethodDescriptorType
-from utilities import download_latest, git_push
+from utilities import download_latest, refresh_markdown
 import argparse
 import os
-from mdutils.mdutils import MdUtils
-from mdutils import Html
 # %%
 if __name__ == "__main__":
 
@@ -33,33 +31,5 @@ if __name__ == "__main__":
                     server= args.server,
                     delete_intermediate_files=args.delete_intermediate_files)
 
-    # delete old markdon file
-    os.remove(os.path.join(args.blog_dir, "_pages", "hometown.md"))
 
-    # delete old image
-    for f in os.listdir(os.path.join(args.blog_dir, "_pages")):
-        if f.endswith(".png") and f != new_image_path:
-            os.remove(os.path.join(args.blog_dir, "_pages", "hometown.md"))
-
-  # %% 
-    # crate new markdown file referencing the image, inside correct directory
-    mdFile = MdUtils(file_name=os.path.join(args.blog_dir, "_pages", "hometown.md"), title='Mação')
-    img_date = os.path.basename(new_image_path).split("_")[0]
-
-    mdFile.new_paragraph(f"This is a satellite picture of Mação, my home region, taken on {img_date}. It is the latest available image from ESA's Sentinel 2 satellites. It's kept updated using some [scripts](https://github.com/fernandeslouro/terras) I made to have something on my website to mark the passage of time. It all runs on a rented server I also use to self-host some services.")
-
-    mdFile.new_paragraph("In the summer months, the picture will appear very brown. Most of the area was burned in recent wildfires, and it shows when the grass dies. In winter, it turns greener, but there's not a lot of forest now. The population is also shrinking at an alarming pace.")
-
-    mdFile.new_paragraph("I grew up here, and I have love for this land. You should visit if you have the chance.")
-
-    mdFile.new_paragraph(" ")
-    mdFile.new_paragraph(" ")
-
-    image_text = f"Mação viewed from the sky at {img_date}"
-    mdFile.new_line(mdFile.new_inline_image(text=image_text, path=os.path.join("/assets/images", os.path.basename(new_image_path))))
-
-    mdFile.create_md_file()
-# %%
-    # commit and push to blog
-    git_push(os.path.join(args.blog_dir, ".git"),
-        f'Updating with image from {new_image_path.split("-")[0]}')
+    refresh_markdown(new_image_path, args.blog_dir)
